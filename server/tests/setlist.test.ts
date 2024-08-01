9d42a16569f8a818ae215da9e539d68426554e00
Camya Robinson <camyacodes@gmail.com>
2024-08-01 00:41:01 -0400

    server setup with typescript and eslint

---
+++ server/tests/setlist.test.ts
@@ -0,0 +1,11 @@
+// songs can be removed
+// delete song
+
+// songs can be added
+// post song
+
+// songs can be reorganized
+// put setlist
+
+// songs can be updated
+// put song
---
+++ server/tests/user.test.ts
@@ -0,0 +1,13 @@
+// users can sign up
+// post user
+
+// users can login in
+// post user
+
+// users can logout
+// put user
+
+// users can create a setlist
+// post setlist
+
+// delete setlist
